7fcd93f3854414327a01854d442a391bc2a76f56
sdorbala <logik6@gmail.com>
2020-01-16 12:52:24 -0800

    BalancedBST begin

--- challenges/BalancedBST.cs
+++ challenges/BalancedBST.cs
@@ -1,3 +1,10 @@
+using System;
 /* 
     Given a sorted array, convert it into a height-balanced binary search tree.
 */
+
+namespace challenges.DCP {
+    public static class BalancedBST {
+
+    }
+}
